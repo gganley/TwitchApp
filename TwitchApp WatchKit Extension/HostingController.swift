f5f66d41ae6e9644a1232a3ed204c333b246569a
Gregory Ganley <gganley@student.bridgew.edu>
2019-06-05 14:15:15 -0400

    Initial Commit

---
+++ TwitchApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  TwitchApp WatchKit Extension
+//
+//  Created by Gregory Ganley on 2019-06-05.
+//  Copyright © 2019 Gregory Ganley. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ TwitchApp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  TwitchApp WatchKit Extension
+//
+//  Created by Gregory Ganley on 2019-06-05.
+//  Copyright © 2019 Gregory Ganley. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews : PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
